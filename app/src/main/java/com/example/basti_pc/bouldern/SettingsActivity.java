ecb2e57809d0cd47e701962cf1dfaf0562f5d5ec
Bastian Krammer <Bastian.Krammer@haw-hamburg.de>
2018-11-10 11:56:36 +0100

    Preference Activity added

--- app/src/main/java/com/example/basti_pc/bouldern/SettingsActivity.java
+++ app/src/main/java/com/example/basti_pc/bouldern/SettingsActivity.java
@@ -1,11 +1,13 @@
 package com.example.basti_pc.bouldern;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-public class SettingsActivity extends PreferenceFragmentCompat {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.preferences, rootKey);
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_settings);
     }
 }
